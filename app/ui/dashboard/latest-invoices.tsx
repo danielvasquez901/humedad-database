dc5d0398622cac7137016fa546ca53fadfef6162
daniel <danielv.012003@gamil.com>
2024-10-28 15:41:24 -0500

    Eliminar referencia a lusitana en LatestInvoices

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,8 +1,10 @@
 import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
+// import { lusitana } from '@/app/ui/fonts'; // Esta línea está comentada
+
 import { LatestInvoice } from '@/app/lib/definitions';
+
 export default async function LatestInvoices({
   latestInvoices,
 }: {
@@ -10,13 +12,13 @@ export default async function LatestInvoices({
 }) {
   return (
     <div className="flex w-full flex-col md:col-span-4">
-      <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+      <h2 className="mb-4 text-xl md:text-2xl"> {/* Eliminada la referencia a lusitana.className */}
         Latest Invoices
       </h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
         {/* NOTE: Uncomment this code in Chapter 7 */}
-
-        {/* <div className="bg-white px-6">
+        {/* 
+        <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
@@ -45,18 +47,17 @@ export default async function LatestInvoices({
                     </p>
                   </div>
                 </div>
-                <p
-                  className={`${lusitana.className} truncate text-sm font-medium md:text-base`}
-                >
+                <p className="truncate text-sm font-medium md:text-base"> {/* Eliminada la referencia a lusitana.className */}
                   {invoice.amount}
                 </p>
               </div>
             );
           })}
-        </div> */}
+        </div> 
+        */}
         <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
-          <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
+          <h3 className="ml-2 text-sm text-gray-500">Updated just now</h3>
         </div>
       </div>
     </div>
