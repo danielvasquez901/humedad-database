0ceb30502fa5e1ff09d0beb3a89cd3f76a3ba340
daniel <danielv.012003@gamil.com>
2024-10-28 15:31:31 -0500

    Eliminar referencia a lusitana en Cards

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -4,7 +4,7 @@ import {
   UserGroupIcon,
   InboxIcon,
 } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+// import { lusitana } from '@/app/ui/fonts';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -17,7 +17,6 @@ export default async function CardWrapper() {
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
-
       {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
@@ -47,10 +46,7 @@ export function Card({
         {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
-      <p
-        className={`${lusitana.className}
-          truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
-      >
+      <p className="truncate rounded-xl bg-white px-4 py-8 text-center text-2xl"> {/* Eliminada la referencia a lusitana */}
         {value}
       </p>
     </div>
